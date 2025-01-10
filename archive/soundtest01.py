650ee4a3190d25f341c31a04e63fb26a998f9ce5
Bob Harper <msutter@cogeco.ca>
2025-01-10 00:11:19 -0500

    added conversation history.

--- archive/soundtest01.py
+++
@@ -1,5 +0,0 @@
-from robot_hat import Music
-
-music = Music()
-
-music.sound_play("/usr/share/sounds/alsa/Front_Center.wav")
--- archive/tts_test.py
+++
@@ -1,4 +0,0 @@
-from robot_hat import TTS
-
-tts = TTS()
-tts.say("Testing sound without sudo!")
