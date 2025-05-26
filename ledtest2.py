549dd7c3066b6f8b7d4279145ba1b66ad17a3144
Bob Harper <msutter@cogeco.ca>
2025-05-26 00:28:29 -0400

    Refactor: Update LED test scripts for improved brightness control; enhance startup sequence with default expression setting

--- ledtest2.py
+++ ledtest2.py
@@ -4,9 +4,19 @@ import time
 motors = Motors(db="./motors_override.db")
 motor_id = 1
 
-motors[motor_id].speed(10)
+motors[motor_id].speed(1)
 print("[DEBUG] Set LED power to 30%")
-time.sleep(5)
+time.sleep(3)
 
+motors[motor_id].speed(0.12)
+print("[DEBUG] Set LED power to 30%")
+time.sleep(3)
+
+motors[motor_id].speed(0)
+print("[DEBUG] Set LED power to 30%")
+time.sleep(3)
+motors[motor_id].speed(5)
+print("[DEBUG] Set LED power to 30%")
+time.sleep(3)
 motors[motor_id].speed(0)
 print("[DEBUG] Turned LED OFF")
